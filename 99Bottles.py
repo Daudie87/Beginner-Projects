eed93687394ecfcf0b07a8b41fcd0f9d8b852efc
SmartphonE <pratyakshsoni2004@gmail.com>
2020-12-26 12:16:18 +0530

    Update 99Bottles.py
    Does a little bit beautification so that it gives a cool output with number of steps which makes it really look good.
    
    How is it ?

--- 99Bottles.py
+++ 99Bottles.py
@@ -1,33 +1,35 @@
-def firstVerse(count, bottleTense):
-    firstVerse = "%s %s of beer on the wall, %s %s of beer." % (count, bottleTense, count, bottleTense)
+def firstVerse(step,count, bottleTense):
+    firstVerse = "Step : %s, \t%s %s of beer on the wall, %s %s of beer." % (step,count, bottleTense, count, bottleTense)
     print(firstVerse)
 
-def secondVerse(count, bottleTense):
-    secondVerse = "Take one down and pass it around, %s %s of beer on the wall!\n" % (count, bottleTense)
+def secondVerse(step,count, bottleTense):
+    secondVerse = " \t\t\tTake one down and pass it around, %s %s of beer on the wall!\n" % (count, bottleTense)
     print(secondVerse)
 
 def lyrics():
+    steps = 1
     count = 99
     bottleTense = "bottles"
 
     while count > 1:
-        firstVerse(count, bottleTense)
+        firstVerse(steps,count, bottleTense)
+        steps+=1
         count -= 1
 
         if count > 1:
-            secondVerse(count, bottleTense)
+            secondVerse(steps,count, bottleTense)
         elif count == 1:
             bottleTense = "bottle"
-            secondVerse(count, bottleTense)
+            secondVerse(steps,count, bottleTense)
 
-    firstVerse(count, bottleTense)
+    firstVerse(steps,count, bottleTense)
 
     count = "no more"
     bottleTense = "bottles"
-    secondVerse(count, bottleTense)
+    secondVerse(steps,count, bottleTense)
 
     count = "No more"
-    firstVerse(count, bottleTense)
-    print("Go to the store and buy some more, 99 bottles of beer on the wall!\n")
+    firstVerse(steps,count, bottleTense)
+    print("\t\t\tGo to the store and buy some more, 99 bottles of beer on the wall!\n")
 
 lyrics()
