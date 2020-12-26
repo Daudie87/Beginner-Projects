6fea57b22b6a0136e68aed4130a0e285db4e7bf4
SmartphonE <pratyakshsoni2004@gmail.com>
2020-12-26 12:49:15 +0530

    Update coinEstimator.py
    That was incomplete so i completed the code , beautified it , implemented logic .
    
    now it will tell you weight of all coins also total weight caused by them in your pocket :angel: :+1:
    
    How is it ?

--- coinEstimator.py
+++ coinEstimator.py
@@ -4,16 +4,16 @@ nickelWeight = 5.0
 dimeWeight = 2.268
 quarterWeight = 5.670
 
-# amount of coins that fit in each type of wrapper
-pennyWrapper = 50
-nickelWrapper = 40
-dimeWrapper = 50
-quarterWrapper = 40
-
 print("Welcome to the Coin Estimator!")
-print("Please enter the weight of your: ")
+print("Please enter the weight in grams of your: ")
 
 pennies = float(input("Pennies: "))
 nickels = float(input("Nickels:" ))
 dimes = float(input("Dimes: "))
 quarters = float(input("Quarters: "))
+
+print(f"\nYour pennies weigths : {pennies*pennyWeight} gm")
+print(f"Your nickles weigths : {nickels*nickelWeight} gm")
+print(f"Your dimes weigths : {dimes*dimeWeight} gm")
+print(f"Your quarter weigths : {quarters*quarterWeight} gm")
+print(f"\nYour Total pocket weights : {(quarters*quarterWeight) +(pennies*pennyWeight)+(nickels*nickelWeight)+(dimes*dimeWeight)} Grams")
